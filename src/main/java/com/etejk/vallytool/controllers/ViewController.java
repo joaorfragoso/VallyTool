5093d2a04ce907fad71fb7fdad673384a357d0a5
Marcos Guimarães <marcosguimaraest04@gmail.com>
2022-09-09 19:13:27 -0300

    Removida conexão com banco de dados

--- src/main/java/com/etejk/vallytool/controllers/ViewController.java
+++ src/main/java/com/etejk/vallytool/controllers/ViewController.java
@@ -1,18 +1,11 @@
 package com.etejk.vallytool.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.etejk.vallytool.dto.RequisicaoNovoUsuario;
-import com.etejk.vallytool.services.UsuarioService;
 
 @Controller
 public class ViewController {
-	
-	@Autowired
-	public UsuarioService service;
 	
 	@GetMapping("")
 	public String redirect() {
@@ -20,13 +13,12 @@ public class ViewController {
 	}
 	
 	@GetMapping("register")
-	public String register(RequisicaoNovoUsuario usuario) {
+	public String register() {
 		return "register";
 	}
 	
 	@GetMapping("usuarios")
-	public String usuarios(Model model) {
-		model.addAttribute("usuarios", service.findUsuarios());
+	public String usuarios() {
 		return "usuarios";
 	}
 	
