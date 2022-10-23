91854e007f05da4456dac9c7ba49310bbd6a9354
Marcola <marcosguimaraest04@gmail.com>
2022-10-23 19:08:20 -0300

    Botão de pesquisar turmas e usuarios funcionando

--- src/main/java/com/etejk/vallytool/repositories/TurmaRepository.java
+++ src/main/java/com/etejk/vallytool/repositories/TurmaRepository.java
@@ -1,11 +1,18 @@
 package com.etejk.vallytool.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.etejk.vallytool.entities.Turma;
 
 @Repository
 public interface TurmaRepository extends JpaRepository<Turma, Integer>{
+	
+	@Query("SELECT u FROM Turma u WHERE u.codigo LIKE %?1%")
+	List<Turma> search(String search);
+	
 	Turma findByCodigo(String codigo);
 }
--- src/main/java/com/etejk/vallytool/repositories/UsuarioRepository.java
+++ src/main/java/com/etejk/vallytool/repositories/UsuarioRepository.java
@@ -1,7 +1,9 @@
 package com.etejk.vallytool.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.etejk.vallytool.entities.Usuario;
@@ -9,5 +11,8 @@ import com.etejk.vallytool.entities.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
 	Usuario findByCpf(String cpf);
+	
+	@Query("SELECT u FROM Usuario u WHERE u.nome LIKE %?1%")
+	List<Usuario> search(String search);
 	
 }
