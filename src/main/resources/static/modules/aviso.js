1b1918e53b4a11d408707b99ecef2cf472ffa528
Joao Fragoso <joaovrfragoso@gmail.com>
2022-10-25 19:01:46 -0300

    Edição na mensagem de erro na pagina de avaliar turma, e implementação da janela de aviso nela

--- src/main/resources/static/modules/aviso.js
+++ src/main/resources/static/modules/aviso.js
@@ -1,4 +1,4 @@
 let parametrosString = window.location.search.substring(1);
 if(parametrosString.includes("error")) {
-	document.getElementsByClassName("aviso")[0].style.display = "flex";
+	document.getElementsByClassName("aviso")[0].style.display = "grid";
 }
