adc5110d862a57c4ed1a4f5e74e039a145f4fc60
Marcola <marcosguimaraest04@gmail.com>
2022-10-29 23:59:05 -0300

    Ajustes avaliar

--- src/main/java/com/etejk/vallytool/controllers/AvaliarController.java
+++ src/main/java/com/etejk/vallytool/controllers/AvaliarController.java
@@ -3,6 +3,7 @@ package com.etejk.vallytool.controllers;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +21,7 @@ import com.etejk.vallytool.entities.Usuario;
 import com.etejk.vallytool.repositories.CompetenciaRepository;
 import com.etejk.vallytool.repositories.DisciplinaRepository;
 import com.etejk.vallytool.repositories.ProfessoRepository;
+import com.etejk.vallytool.repositories.RelacaoRepository;
 import com.etejk.vallytool.repositories.ResultadoRepository;
 import com.etejk.vallytool.repositories.TurmaRepository;
 import com.etejk.vallytool.services.ResultadoService;
@@ -46,12 +48,18 @@ public class AvaliarController {
 	@Autowired
 	private ResultadoService rs;
 
+	@Autowired
+	private RelacaoRepository rer;
+	
 	@GetMapping("avaliar")
-	public String avaliar(Model model, Authentication auth) {
+	public String avaliar(Model model, Authentication auth,
+			@Param(value = "turma") String turma) {
 		Usuario usuario = pr.findByNome(auth.getName());
 		model.addAttribute("competencias", cr.findAll());
 		model.addAttribute("turmas", usuario.getTurmas());
 		model.addAttribute("disciplinas", usuario.getDisciplinas());
+		System.out.println(turma);
+		model.addAttribute("relacoes", rer.findByUsuario(usuario));
 		model.addAttribute("usuario", usuario);
 		return "site/avaliar_turma";
 	}
