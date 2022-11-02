e8ea3412d70241ccec762e7f27759f6ae6cf952f
Marcola <marcosguimaraest04@gmail.com>
2022-11-02 14:12:53 -0300

    Erro enviado caso aconteça algo de errado no envio de email

--- src/main/java/com/etejk/vallytool/controllers/RedefinirController.java
+++ src/main/java/com/etejk/vallytool/controllers/RedefinirController.java
@@ -56,8 +56,12 @@ public class RedefinirController {
 		rs.createPasswordToken(token, user);
 		
 		String url = "http://localhost:8080" + "/redefinir/atualizarSenha?token=" + token;
+		try {
 		es.sendEmail(user.getEmail(), url);
-		
+		}catch(Exception e) {
+			model.addAttribute("error", "Algo deu errado, tente novamente.");
+			return new ModelAndView("redirect:/redefinir", model);
+		}
 		model.addAttribute("sucess", "Um email foi enviado para você contendo as informações sobre a redefinição de senha");
 		model.addAttribute("email", user.getEmail());
 		return new ModelAndView("redirect:/redefinir", model);
